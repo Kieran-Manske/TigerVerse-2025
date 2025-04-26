9f594c9909b47034848166ee21d249495745ffbe
Kieran.Manske <kieran.manske@gmail.com>
2025-04-26 15:19:18 -0500

    Basic player movement frame

--- Assets/Player/scripts/VRPlayerController.cs
+++ Assets/Player/scripts/VRPlayerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // for new Input System (if you use it)
 
-public class VRPlayerController : MonoBehaviour
+public class VRPlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 1.5f;
+    public Transform headTransform; // assign your Main Camera here
+    private CharacterController characterController;
+    
+    private Vector2 inputAxis;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Read input from the left controller
+        inputAxis = new Vector2(
+            Input.GetAxis("XRI_Left_Primary2DAxis_Horizontal"),
+            Input.GetAxis("XRI_Left_Primary2DAxis_Vertical")
+        );
         
+        Vector3 move = headTransform.forward * inputAxis.y + headTransform.right * inputAxis.x;
+        move.y = 0; // prevent flying
+        characterController.Move(move * moveSpeed * Time.deltaTime);
     }
 }
